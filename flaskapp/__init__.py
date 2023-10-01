fdfc0c98fb891ccc09de0d46883298e0ba6f6f13
Mahfuz <mahfuzurrahman4414@gmail.com>
2023-10-02 03:24:38 +0600

    Registration

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,5 +1,15 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from flaskapp.routes import app
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,11 +1,23 @@
-from flask import render_template
-from flaskapp import app
+from flaskapp import app, db, bcrypt
+from flask import render_template, url_for, flash, redirect, request
+from flaskapp.forms import SignUp
+from flaskapp.db_models import User
 
 @app.route('/')
 @app.route('/home/')
 def home():
 	return render_template('home.html')
 
-@app.route('/about/')
-def about():
-	return render_template('about.html', title='About')
+@app.route('/signup/', methods=['GET', 'POST'])
+def signup():
+	print(User.query.all())
+	form = SignUp(request.form)
+	if request.method == 'POST' and form.validate():
+		hashed_pass = bcrypt.generate_password_hash(form.password.data, rounds=13).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_pass)
+		db.session.add(user)
+		db.session.commit()
+		flash(f'Account created for {form.username.data}', 'success')
+		return redirect(url_for('home'))
+	return render_template('sign_up.html', title='Sign Up', form=form)
+
