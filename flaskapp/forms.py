8376130def58924158dc16764ee0e99ac7b2b99e
Mahafuzur Rahaman <62833292+mahfuz-prog@users.noreply.github.com>
2023-10-04 02:27:17 +0600

    Update forms.py

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Email
 from flaskapp.user_login import users
 
 class LogIn(FlaskForm):
-	email = StringField('User email', validators=[DataRequired()])
+	email = StringField('User email', validators=[DataRequired(), Email()])
 	password = PasswordField('User password', validators=[DataRequired()])
 	remember = BooleanField('Remember me')
 	submit = SubmitField('Login')
